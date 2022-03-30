70582a28027ef4a39b7759421b684952c5bfac62
alexpharris <alexpharris79@gmail.com>
2022-03-30 18:45:33 -0500

    Remove unused observable

--- frontend/src/scripts/test/wordset.ts
+++ frontend/src/scripts/test/wordset.ts
@@ -1,5 +1,4 @@
 import { randomElementFromArray, randomIntFromRange } from "../utils/misc";
-import * as WordsetRetrievedEvent from "../observables/wordset-retrieved-event";
 
 let currentWordset: Wordset | null = null;
 let currentWordGenerator: WordGenerator | null = null;
@@ -105,7 +104,6 @@ export function withWords(words: string[], funbox: string): Wordset {
     if (currentWordset == null || words !== currentWordset.words) {
       currentWordset = new Wordset(words);
     }
-    WordsetRetrievedEvent.dispatch(currentWordset);
     return currentWordset;
   }
 }
